2353cb407696184962911daef282ffb5753c5927
The_better_duck <98641176+meephubub@users.noreply.github.com>
2025-12-10 20:40:41 +0000

    Update page.tsx

--- app/embed/page.tsx
+++ app/embed/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
     router.replace(`?${params.toString()}`, { scroll: false });
   }, [url, proxy, spoof, hideCursor, router]);
 
-  const finalSrc = proxy && url ? `/api/proxy?url=${encodeURIComponent(url)}` : url;
+  const finalSrc = proxy && url ? `/api/embedproxy?url=${encodeURIComponent(url)}` : url;
 
   return (
     <main className="min-h-screen bg-black text-white flex flex-col">
